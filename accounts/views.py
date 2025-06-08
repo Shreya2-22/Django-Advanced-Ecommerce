52982c2cc81ca9d26f564f39a4d8b070e7946cfc
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-08 22:27:20 +0545

    Add dashboard page

--- accounts/views.py
+++ accounts/views.py
@@ -63,7 +63,7 @@ def login(request):
         if user is not None:
             auth.login(request, user)
             #messages.success(request, "You are now logged in.")
-            return redirect('home')
+            return redirect('dashboard')
         else:
             messages.error(request, "Invalid login credentials.")
             return redirect('login')
@@ -92,3 +92,8 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, 'Activation link is invalid!')
         return redirect('register')
+    
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
