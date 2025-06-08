b300934b158ab74286c8e1b8f00733628411d7b0
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-09 00:25:53 +0545

    update cart and counter for logged in users

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
+import requests
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
@@ -72,15 +73,40 @@ def login(request):
                     for item in cart_item:
                         variation = item.variations.all()
                         product_variation.append(list(variation))
-                        item.user = user
-                        item.save()
-                else:
-                    cart_item = None
+                       
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+                    
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
             except:
                 pass
             auth.login(request, user)
-            #messages.success(request, "You are now logged in.")
-            return redirect('dashboard')
+            messages.success(request, "You are now logged in.")
+            url = request.META.get('HTTP_REFERER')  # Get the URL of the page that referred the user
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, "Invalid login credentials.")
             return redirect('login')
