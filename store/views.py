6d8ec8e4a46956a4590ca1ac306a131211226ca0
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-12 21:16:46 +0545

    update reviews and rating systems

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from carts.models import CartItem
 from carts.views import _cart_id
 from category.models import Category
+from orders.models import OrderProduct
 from .models import Product, ReviewRating
 from .forms import ReviewForm
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
@@ -37,20 +38,31 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request), product= single_product).exists()
-        
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
-    
-    try:
-        orderproduct = Product.objects.get(user=request.user, product_id=single_product.id).exists()
-    except Product.DoesNotExist:
+
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
         orderproduct = None
+
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+
+    # Get the product gallery
+    #product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
-        'single_product':single_product,
-        'in_cart':in_cart,
+        'single_product': single_product,
+        'in_cart'       : in_cart,
         'orderproduct': orderproduct,
-        }
+        'reviews': reviews,
+        #'product_gallery': product_gallery,
+    }
     return render(request, 'store/product_detail.html', context)
 
 
